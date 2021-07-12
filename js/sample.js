f9d4bff8cdbbba74dfc9e5bd8b26b87060a40845
kdgnanbu <71370134+kdgnanbu@users.noreply.github.com>
2021-07-12 13:18:17 +0900

    Add files via upload

---
+++ js/sample.js
@@ -0,0 +1,28 @@
+//$(function() {
+//    $('.single-item').slick();
+//});
+
+
+$(function(){
+  $('.single-item').slick({
+    accessibility: true,
+    autoplay: true,
+    autoplaySpeed: 1800,
+//    dots: true,
+    fade: true,
+  });
+});
+
+//サンプル
+$(function () {
+    $('.button').prevAll().hide();
+    $('.button').click(function () {
+        if ($(this).prevAll().is(':hidden')) {
+            $(this).prevAll().slideDown();
+            $(this).text('閉じる').removeClass('close');
+        } else {
+            $(this).prevAll().slideUp();
+            $(this).text('もっと見る').removeClass('close');
+        }
+    });
+});
